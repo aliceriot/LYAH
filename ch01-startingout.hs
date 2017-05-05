11359decf400112cf787e24aecdbc6cf28cd5452
Alice Pote <alice.writes.wrongs@gmail.com>
2017-05-05 11:19:08 -0400

    hindent

--- ch01-startingout.hs
+++ ch01-startingout.hs
@@ -1,32 +1,39 @@
 --starting LYAH
-
 doubleMe x = x + x
 
-doubleUs x y = x*2 + y*2
+doubleUs x y = x * 2 + y * 2
 
 --doubleUs x y = doubleMe x + doubleMe y
-
-doubleSmallNumber x = if x > 100
-                        then x
-                        else x*2
+doubleSmallNumber x =
+    if x > 100
+        then x
+        else x * 2
 
 -- we have something like cons
-
 consit x y = x : y
 
-
-boomBangs xs = [ if x < 10 then "Boom!" else "Bang!" | x <- xs, odd x]
+boomBangs xs =
+    [ if x < 10
+         then "Boom!"
+         else "Bang!"
+    | x <- xs 
+    , odd x ]
 
 -- list comprehension is really cool in haskell!
 -- better syntax and more powah compared with python, I think
-
 -- we can use it to define length
+length' xs =
+    sum
+        [ 1
+        | _ <- xs ]
 
-length' xs = sum [1 | _ <- xs]
+removeNonUppercase string =
+    [ c
+    | c <- string 
+    , elem c ['A' .. 'Z'] ]
 
-
-removeNonUppercase string = [ c | c <- string, elem c ['A'..'Z']]
-
-
-removeOdds biglist = [ [ x | x <- lat, even x] | lat <- biglist]
-
+removeOdds biglist =
+    [ [ x
+      | x <- lat 
+      , even x ]
+    | lat <- biglist ]
